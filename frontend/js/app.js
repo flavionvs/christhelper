210e708b6b10b3694fbe18727dec20ff73ac81ac
Flavio Neves da Silva <flavionevesdasilva@thecodehelper.com>
2026-04-16 00:41:54 +1200

    Tag

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,6 @@
 const DEFAULT_API_BASE = 'https://api.christhelper.com';
 const GA_MEASUREMENT_ID = 'G-VEBD7Q9H0S';
+const GOOGLE_TAG_ID = 'GT-WFMZSLB3';
 const GA_ALLOWED_EVENTS = new Set([
   'page_view',
   'sign_up',
@@ -41,7 +42,7 @@ function loadGoogleAnalytics() {
 
   const script = document.createElement('script');
   script.async = true;
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(GA_MEASUREMENT_ID)}`;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(GOOGLE_TAG_ID || GA_MEASUREMENT_ID)}`;
   script.onload = () => { gaReady = true; };
   document.head.appendChild(script);
 }
